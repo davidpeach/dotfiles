f977c1275f29150ca1ee582c24cc1fcec6a12026
David Peach <mail@davidpeach.co.uk>
2022-05-25 17:11:25 +0100

    Initial install script which just stows some folders

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+joinByChar() {
+  local IFS="$1"
+  shift
+  joined="$*"
+}
+
+declare -a stowFolders=(
+     bash
+     bin
+     dmenu-scripts
+     i3
+     kitty
+     nvim
+     xmonad
+     zsh
+)
+
+joinByChar ' ' "${stowFolders[@]}"
+
+stow --restow $joined
