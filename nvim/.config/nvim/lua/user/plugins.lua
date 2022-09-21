e56d21a9da3492bb58f845abda130d5635d90fc7
David Peach <hello@davidpeach.me>
2022-09-21 14:47:56 +0100

    NVIM :: Add vim rest console plugin

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -190,6 +190,8 @@ return packer.startup(function(use)
         end
     }
 
+    use 'diepm/vim-rest-console'
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
