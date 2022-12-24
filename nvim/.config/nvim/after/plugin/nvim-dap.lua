5cdac033abe2731e2190cfeab27d3e9605057cc7
David Peach <mail@davidpeach.co.uk>
2022-12-24 20:19:50 +0000

    Fix nvim dap and xdebug for use with sail. Sail needs an xdebug file copying into the docker container.

--- nvim/.config/nvim/after/plugin/nvim-dap.lua
+++ nvim/.config/nvim/after/plugin/nvim-dap.lua
@@ -2,7 +2,7 @@ local dap = require('dap')
 dap.adapters.php = {
   type = 'executable',
   command = 'node',
-  args = { "/home/david/.neovim/vscode-php-debug/out/phpDebug.js" }
+  args = { os.getenv("HOME") .. "/nvim/vscode-php-debug/out/phpDebug.js" }
 }
 
 dap.configurations.php = {
@@ -10,7 +10,10 @@ dap.configurations.php = {
     type = 'php',
     request = 'launch',
     name = 'Listen for Xdebug',
-    port = 9003
+    port = 9003,
+    pathMappings = {
+      ["/var/www/html"] = "${workspaceFolder}"
+    }
   }
 }
 
@@ -20,6 +23,7 @@ vim.keymap.set("n", "<F9>", ":lua require'dap'.continue()<CR>")
 vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>")
 vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>")
 vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
+vim.keymap.set("n", "<leader>dg", ":lua require'dapui'.toggle()<CR>")
 
 require('nvim-dap-virtual-text').setup {
     enabled = true
