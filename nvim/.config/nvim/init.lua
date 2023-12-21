7a515b4735b2dea86e6331ffb91d71ad31c427cd
David Peach <mail@davidpeach.co.uk>
2023-12-21 15:57:37 +0000

    update nvim init for intellephense. Remove vimwiki.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -216,8 +216,43 @@ require('lazy').setup({
     end
   },
 
-  'vimwiki/vimwiki',
+  {
+    'preservim/vim-markdown',
+    init = function ()
+      vim.cmd([[
+      " Enable folding.
+      let g:vim_markdown_folding_disabled = 0
 
+      " Fold heading in with the contents.
+      let g:vim_markdown_folding_style_pythonic = 1
+
+      " Don't use the shipped key bindings.
+      let g:vim_markdown_no_default_key_mappings = 1
+
+      " Autoshrink TOCs.
+      let g:vim_markdown_toc_autofit = 1
+
+      " Indentation for new lists. We don't insert bullets as it doesn't play
+      " nicely with `gq` formatting. It relies on a hack of treating bullets
+      " as comment characters.
+      " See https://github.com/plasticboy/vim-markdown/issues/232
+      let g:vim_markdown_new_list_item_indent = 0
+      let g:vim_markdown_auto_insert_bullets = 0
+
+      " Filetype names and aliases for fenced code blocks.
+      let g:vim_markdown_fenced_languages = ['php', 'py=python', 'js=javascript', 'bash=sh', 'viml=vim']
+
+      " Highlight front matter (useful for Hugo posts).
+      let g:vim_markdown_toml_frontmatter = 1
+      let g:vim_markdown_json_frontmatter = 1
+      let g:vim_markdown_frontmatter = 1
+
+      " Format strike-through text (wrapped in `~~`).
+      let g:vim_markdown_strikethrough = 1
+]])
+
+    end
+  }
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -521,7 +556,18 @@ require('mason-lspconfig').setup()
 local servers = {
 
   cssls = {},
-  intelephense = {},
+  intelephense = {
+    filetypes = { "php", "blade" },
+    settings = {
+      intelephense = {
+        filetypes = { "php", "blade" },
+        files = {
+          associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+          maxSize = 5000000,
+        },
+      },
+    },
+  },
   -- clangd = {},
   gopls = {},
   -- pyright = {},
