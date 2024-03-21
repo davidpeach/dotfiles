85946c1cd19238bd45a4c40345220dea736304a3
David Peach <mail@davidpeach.co.uk>
2024-03-21 13:12:12 +0000

    Add zen mode for nvim. 💆

---
+++ nvim/lua/plugins/zen.lua
@@ -0,0 +1,12 @@
+return {
+  -- Using my own fork until this gets merged in:
+  -- https://github.com/folke/zen-mode.nvim/pull/119/commits
+  "davidpeach/zen-mode.nvim",
+  opts = {
+    window = {
+      width = .5,
+      backdrop = 1,
+    },
+  }
+}
+
