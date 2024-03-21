5710c380d1073dfea5e7f6eabfb1ff7d8bc01423
David Peach <mail@davidpeach.co.uk>
2024-03-21 13:11:13 +0000

    Change to nightfly theme for nvim and kitty. 🌚 🪰

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,11 +1,10 @@
 return {
-	'ribru17/bamboo.nvim',
+	"bluz71/vim-nightfly-colors",
+	name = 'nightfly',
 	lazy = false,
 	priority = 1000,
-	config = function()
-		require('bamboo').setup {
-			-- optional configuration here
-		}
-		require('bamboo').load()
-	end,
+	config = function ()
+		vim.cmd [[colorscheme nightfly]]
+		vim.g.nightflyTransparent = true
+	end
 }
