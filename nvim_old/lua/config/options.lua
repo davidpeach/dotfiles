201922d95e2320d49e23608940569728268a4326
David Peach <mail@davidpeach.co.uk>
2024-03-21 16:25:52 +0000

    Delete old nvim configs.

--- nvim_old/lua/config/options.lua
+++
@@ -1,6 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.cmd([[
-  au FileType php set makeprg=./vendor/bin/phpstan\ analyse\ --memory-limit=2G\ --no-progress\ --error-format=raw\ --configuration=phpstan.neon
-]])
--- nvim_old/lua/plugins/disabled.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  { "windwp/nvim-ts-autotag", enabled = false },
-  { "folke/persistence.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-}
