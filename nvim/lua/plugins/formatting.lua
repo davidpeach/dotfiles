8ed446323d1d50f4a2a947d2ee9b7a89ec75db03
David Peach <mail@davidpeach.co.uk>
2024-11-04 20:00:41 +0000

    nvim - fix deprecation warning and stop auto save formatting

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -4,10 +4,6 @@ return {
   config = function()
     require("conform").setup({
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 2000,
-        lsp_fallback = true,
-      },
       formatters_by_ft = {
         lua = { "mystylua" },
         -- php = { "pint" },
